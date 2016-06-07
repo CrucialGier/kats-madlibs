a598d0a2369987090ea6ef5cb662959d9b4d35b3
Katarina Tuttle and Stewart Cole <stewart@>
2016-06-07 11:16:55 -0700

    add forloop

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var placeInput = $("input#place").val();
-    var transportationInput = $("input#transportation").val();
-    var exclamationInput = $("input#exclamation").val();
-    var adjectiveInput = $("input#adjective").val();
-    var animalInput = $("input#animal").val();
+    debugger;
+    var blanks = ['person1', 'person2','place', 'transportation', 'exclamation', 'adjective', 'animal'];
+    blanks.forEach(function(blank) {
+      var userInput = $("#" + blank).val();
+      $("." + blank).text(userInput);
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".place").text(placeInput);
-    $(".transportation").text(transportationInput);
-    $(".exclamation").text(exclamationInput);
-    $(".adjective").text(adjectiveInput);
-    $(".animal").text(animalInput);
-
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var placeInput = $("input#place").val();
+    // var transportationInput = $("input#transportation").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var animalInput = $("input#animal").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".place").text(placeInput);
+    // $(".transportation").text(transportationInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".adjective").text(adjectiveInput);
+    // $(".animal").text(animalInput);
+    });
     $("#story").show();
 
     event.preventDefault();
